eefe7ff981266b0dcc83e0fecf3ebaf76c26ebb1
anacorrea791 <correaanaclara898@gmail.com>
2025-12-04 11:09:42 -0300

    Adiciona novas propriedades à classe Exame
    Foram adicionadas as seguintes propriedades à classe `Exame`:
    - `Descricao`: obrigatória, com mensagem de erro personalizada.
    - `Preco`: obrigatória, com formatação de moeda e nome exibido
      como "Preço de Custo".
    - `PacienteId`: com nome exibido como "Paciente" na interface.

--- Models/Exame.cs
+++ Models/Exame.cs
@@ -3,9 +3,18 @@
     public class Exame
     {
         public int ExameId { get; set; }
-        public string Descricao { get; set; }
-        public decimal Preco { get; set; }
+        [Required(ErrorMessage = "Digite o Nome da Conta")]
 
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "Digite o Nome da Conta")]
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Preço de Custo")]
+        public decimal Preco { get; set; }
+        
+
+        [Display(Name = "Paciente")]
         public int? PacienteId { get; set; }
         public Paciente? Paciente { get; set; }
     }
